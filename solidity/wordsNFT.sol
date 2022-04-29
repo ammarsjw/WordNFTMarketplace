49e99f4d2db8eb7727e000b86d5a071d3bc7e972
ammarsjw <ammarsjw@gmail.com>
2022-04-29 07:42:10 +0500

    WordsNFT.sol updated

--- solidity/wordsNFT.sol
+++ solidity/wordsNFT.sol
@@ -1640,13 +1640,13 @@ contract WordsNFT is ERC721Enumerable, Ownable {
         
         if(marketplaceActive){
             require(marketplaceContractAddress != address(0), "Set marketplace contract address before minting");
-            _mint(marketplaceContractAddress, mintIndex);
+            _safeMint(marketplaceContractAddress, mintIndex);
             _setTokenURI(mintIndex, uri);
             marketplaceContract.setOnAuction(payable(recipient), mintIndex);
             emit Mint(recipient, mintIndex, marketplaceContractAddress, word, uri);
         }
         else{
-            _mint(recipient, mintIndex);
+            _safeMint(recipient, mintIndex);
             _setTokenURI(mintIndex, uri);
             emit Mint(recipient, mintIndex, recipient, word, uri);
         }
