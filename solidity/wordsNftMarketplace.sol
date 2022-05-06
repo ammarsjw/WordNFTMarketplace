dafb30bccb4498ea6be730eabe8cf54e1efdefdf
wasif28 <wasif28@hotmail.com>
2022-05-06 13:18:48 +0500

    reentrancy added

--- solidity/wordsNftMarketplace.sol
+++ solidity/wordsNftMarketplace.sol
@@ -641,6 +641,67 @@ interface IERC721Receiver {
 
 // File: nftMarketplace.sol
 
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and making it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
 
 interface IWordsNFT is IERC721{
 
@@ -648,10 +709,10 @@ interface IWordsNFT is IERC721{
 
 }
 
-contract WordsNFTMarketplace is Ownable, IERC721Receiver {
+contract WordsNFTMarketplace is Ownable, IERC721Receiver, ReentrancyGuard {
     using SafeMath for uint256;
 
-    address payable marketplaceFeeWallet;
+    address payable public marketplaceFeeWallet;
 
     uint8 public marketplacePercentage;
     uint8 public minterPercentage;
@@ -683,9 +744,9 @@ contract WordsNFTMarketplace is Ownable, IERC721Receiver {
     mapping(uint256 => uint256) public lengthForAllBids;
     mapping(uint256 => CurrentBid) public tokenIdForCurrentBid;
 
-    IERC20 WETH;
+    IERC20 public WETH;
 
-    IWordsNFT wordsNFT;
+    IWordsNFT public wordsNFT;
 
     // Events
 
@@ -741,7 +802,7 @@ contract WordsNFTMarketplace is Ownable, IERC721Receiver {
     // Constructor
 
     constructor() {
-        marketplaceFeeWallet = payable(0x0000000000000000000000000000000000000000); // TODO
+        marketplaceFeeWallet = payable(msg.sender); // TODO
 
         marketplacePercentage = 50;
         minterPercentage = 50;
@@ -810,7 +871,7 @@ contract WordsNFTMarketplace is Ownable, IERC721Receiver {
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    function bid(uint256 _newBid, uint256 _tokenId) external {
+    function bid(uint256 _newBid, uint256 _tokenId) external nonReentrant {
         uint256 allowance = WETH.allowance(msg.sender, address(this));
         WordInfo memory tempWordInfo = tokenIdForWordInfo[_tokenId];
         uint256 currentHighestBid = tokenIdForCurrentBid[_tokenId].currentBidAmount;
@@ -846,7 +907,7 @@ contract WordsNFTMarketplace is Ownable, IERC721Receiver {
         }
     }
 
-    function cancelBid(address payable _bidder, uint256 _bidAmount, uint256 _tokenId) external {
+    function cancelBid(address payable _bidder, uint256 _bidAmount, uint256 _tokenId) external nonReentrant {
         require(_bidAmount > 0.01 ether, "cancelBid::No such bid exists");
         require(msg.sender == _bidder, "cancelBid::Only bidder can cancel their own bids");
 
@@ -901,7 +962,7 @@ contract WordsNFTMarketplace is Ownable, IERC721Receiver {
         emit BidCancelled(tempBidder, tempBidAmount, _tokenId);
     }
 
-    function claim(uint256 _tokenId) external {
+    function claim(uint256 _tokenId) external nonReentrant {
         WordInfo memory tempWordInfo = tokenIdForWordInfo[_tokenId];
         uint256 tempBidAmount = tokenIdForCurrentBid[_tokenId].currentBidAmount;
         address payable tempBidder = tokenIdForCurrentBid[_tokenId].currentBidder;
@@ -951,7 +1012,9 @@ contract WordsNFTMarketplace is Ownable, IERC721Receiver {
         }
     }
     
-    receive() external payable {}
+    receive() external payable {
+        
+    }
 
     modifier onlyContract {
         require(msg.sender == address(wordsNFT), "onlyContract::Only WordsNFT Contract can call this function");
